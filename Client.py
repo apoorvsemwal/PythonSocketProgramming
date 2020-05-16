13066b41b7812f953559ad41870b74c70916285a
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-05-16 01:03:01 -0400

    FileReading-MenuDisplay and PrintReport formatting.

--- Client.py
+++ Client.py
@@ -2,17 +2,92 @@
 import socket
 import sys
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
+def display_menu_and_get_user_input():
+    print("Python DB Menu \n")
+    print("1. Find customer")
+    print("2. Add customer")
+    print("3. Delete customer")
+    print("4. Update customer age")
+    print("5. Update customer address")
+    print("6. Update customer phone")
+    print("7. Print report")
+    print("8. Exit \n")
+    inp = input("Select: ")
+    return inp
 
-    # Receive data from the server and shut down
-    received = str(sock.recv(1024), "utf-8")
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+def establish_client_connection_to_server():
+    HOST, PORT = "localhost", 9999
+    data = " ".join(sys.argv[1:])
+
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(data + "\n", "utf-8"))
+
+        # Receive data from the server and shut down
+        received = str(sock.recv(1024), "utf-8")
+
+    print("Sent:     {}".format(data))
+    print("Received: {}".format(received))
+
+
+def find_customer():
+    pass
+
+
+def add_customer():
+    pass
+
+
+def delete_customer():
+    pass
+
+
+def update_customer_age():
+    pass
+
+
+def update_customer_address():
+    pass
+
+
+def update_customer_phone():
+    pass
+
+
+def print_report():
+    pass
+
+
+def exit_code():
+    print("Good bye")
+    return 8
+
+
+def invalid_choice():
+    print("Invalid Choice!!!")
+
+
+def process_user_input(inp):
+    user_input = {'1': find_customer,
+                  '2': add_customer,
+                  '3': delete_customer,
+                  '4': update_customer_age,
+                  '5': update_customer_address,
+                  '6': update_customer_phone,
+                  '7': print_report,
+                  '8': exit_code
+                  }
+    return user_input.get(inp, invalid_choice)()
+
+
+if __name__ == "__main__":
+    # establish_client_connection_to_server()
+    while True:
+        inp = display_menu_and_get_user_input()
+        res = process_user_input(inp)
+        if res == 8:
+            break
--- Server.py
+++ Server.py
@@ -1,6 +1,107 @@
 # Basic starter code taken from: https://docs.python.org/3/library/socketserver.html
 import socketserver
 
+PYTHON_DB = dict()
+
+
+def is_valid_record(split_record):
+    if split_record[0].strip().isnumeric():
+        return False
+    return True
+    # if len(split_record) == 2:
+    #     check_age()
+    # elif len(split_record) == 3:
+    #     check_age()
+    #     check_address()
+    # elif len(split_record) == 4:
+    #     check_age()
+    #     check_address()
+    #     check_phone()
+
+
+def add_record_to_db(split_record):
+    customer_details = []
+    for i in range(1, len(split_record)):
+        customer_details.append(split_record[i].strip())
+    PYTHON_DB[split_record[0].strip()] = customer_details
+
+
+def load_data_in_memory():
+    data_file = open("data.txt", "r")
+    records = data_file.readlines()
+    for record in records:
+        split_record = record.split("|", 3)
+        valid_record = is_valid_record(split_record)
+        if valid_record:
+            add_record_to_db(split_record)
+    return 0
+
+
+def create_server():
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
+
+
+# def check_age():
+#     pass
+#
+#
+# def check_address():
+#     pass
+#
+#
+# def check_phone():
+#     pass
+def find_customer():
+    pass
+
+
+def add_customer():
+    pass
+
+
+def delete_customer():
+    pass
+
+
+def update_customer_age():
+    pass
+
+
+def update_customer_address():
+    pass
+
+
+def update_customer_phone():
+    pass
+
+
+def print_report():
+    sorted_data = []
+    for customer in sorted(PYTHON_DB.keys()):
+        record = [customer]
+        record.extend(PYTHON_DB[customer])
+        sorted_data.append(record)
+
+    dash = '-' * 90
+    print(dash)
+    print('{:<25s}{:<10s}{:<40s}{:<12s}'.format("Name", "Age", "Street Address", "Phone #"))
+    print(dash)
+    for i in range(len(sorted_data)):
+        print('{:<25s}{:<10s}{:<40s}{:<12s}'.format(sorted_data[i][0], sorted_data[i][1], sorted_data[i][2],
+                                                    sorted_data[i][3]))
+
+
+if __name__ == "__main__":
+    load_data_in_memory()
+    print_report()
+    # create_server()
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
@@ -18,13 +119,3 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print(self.data)
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
-
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-
-    # Create the server, binding to localhost on port 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        server.serve_forever()
