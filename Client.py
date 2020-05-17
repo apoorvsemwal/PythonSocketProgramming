a734b872f5fff0f9ccfa6548a84518d885d50edf
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-05-17 15:09:24 -0400

    Find-Add-Delete-PrintReport Implemented

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 # Basic starter code taken from: https://docs.python.org/3/library/socketserver.html
 import socket
-import sys
+
+HOST, PORT = "localhost", 9999
 
 
 def display_menu_and_get_user_input():
@@ -17,61 +18,103 @@ def display_menu_and_get_user_input():
     return inp
 
 
-def establish_client_connection_to_server():
-    HOST, PORT = "localhost", 9999
-    data = " ".join(sys.argv[1:])
-
+def get_socket_to_server():
     # Create a socket (SOCK_STREAM means a TCP socket)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        # Connect to server and send data
-        sock.connect((HOST, PORT))
-        sock.sendall(bytes(data + "\n", "utf-8"))
-
-        # Receive data from the server and shut down
-        received = str(sock.recv(1024), "utf-8")
-
-    print("Sent:     {}".format(data))
-    print("Received: {}".format(received))
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-def find_customer():
-    pass
+def do_client_send_receive(sock, data):
+    try:
+        sock.sendall(str(data).encode())
+        res = eval(sock.recv(4096).decode('utf-8'))
+        return res
+    except:
+        print('Issues connecting to server...')
+
+
+def find_customer(sock):
+    cust_name = input("Enter the customer name to find: ")
+    data = ['1', cust_name]
+    do_client_send_receive(sock, data)
+    if isinstance(res, list):
+        print('Customer record for: ' + cust_name)
+        for idx, val in enumerate(res):
+            if idx == 0:
+                print('Age - ' + val)
+            elif idx == 1:
+                print('Address - ' + val)
+            elif idx == 2:
+                print('Phone - ' + val)
+    else:
+        print(res)
+    print("\n")
 
 
 def add_customer():
-    pass
+    cust_name = input("Enter the customer name to add: ")
+    cust_age = input("Enter customer's age: ")
+    cust_address = input("Enter customer's address: ")
+    cust_phone = input("Enter customer's phone number: ")
+    cust_data = [2, [cust_name, cust_age, cust_address, cust_phone]]
+    res = do_client_send_receive(sock, cust_data)
+    if isinstance(res, list):
+        print('New customer ' + cust_name + ' successfully added to DB.')
+    else:
+        print(res)
+    print("\n")
 
 
-def delete_customer():
-    pass
+def delete_customer(sock):
+    cust_name = input("Enter the customer name to delete: ")
+    cust_data = [3, cust_name]
+    res = do_client_send_receive(sock, cust_data)
+    if isinstance(res, list):
+        print('Customer ' + cust_name + ' successfully deleted from DB.')
+    else:
+        print(res)
+    print("\n")
 
 
 def update_customer_age():
-    pass
+    return 4
 
 
 def update_customer_address():
-    pass
+    return 5
 
 
 def update_customer_phone():
-    pass
+    return 6
 
 
-def print_report():
-    pass
+def print_report(sock):
+    res = do_client_send_receive(sock, [7])
+    if isinstance(res, list) and len(res) > 0:
+        dash = '-' * 90
+        print(dash)
+        print('{:<25s}{:<10s}{:<40s}{:<12s}'.format("Name", "Age", "Street Address", "Phone #"))
+        print(dash)
+        for i in range(len(res)):
+            print('{:<25s}{:<10s}{:<40s}{:<12s}'.format(res[i][0], res[i][1], res[i][2],
+                                                        res[i][3]))
+    else:
+        print('No records in DB to show...')
+    print("\n")
 
 
-def exit_code():
-    print("Good bye")
-    return 8
+def exit_code(sock):
+    print("Good bye!!!!")
+    data = ['8', 'Client said Good bye!!!!']
+    sock.sendall(str(data).encode())
+    res = eval(sock.recv(4096).decode('utf-8'))
+    return res
 
 
 def invalid_choice():
     print("Invalid Choice!!!")
 
 
-def process_user_input(inp):
+def process_user_input(inp, sock):
     user_input = {'1': find_customer,
                   '2': add_customer,
                   '3': delete_customer,
@@ -81,13 +124,19 @@ def process_user_input(inp):
                   '7': print_report,
                   '8': exit_code
                   }
-    return user_input.get(inp, invalid_choice)()
+    return user_input.get(inp, invalid_choice)(sock)
+
+
+def start_client():
+    sock = get_socket_to_server()
+    sock.connect((HOST, PORT))
+    while True:
+        inp = display_menu_and_get_user_input()
+        res = process_user_input(inp, sock)
+        if res and res[0] == '8':
+            sock.close()
+            break
 
 
 if __name__ == "__main__":
-    # establish_client_connection_to_server()
-    while True:
-        inp = display_menu_and_get_user_input()
-        res = process_user_input(inp)
-        if res == 8:
-            break
+    start_client()
--- Server.py
+++ Server.py
@@ -4,19 +4,100 @@ import socketserver
 PYTHON_DB = dict()
 
 
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        print('New client communication started....')
+        while True:
+            data = eval(self.request.recv(4096).decode('utf-8'))
+            if data[0] == '1':
+                res = find_customer(data[1])
+            if data[0] == '2':
+                res = add_customer(data[1])
+            if data[0] == '3':
+                res = delete_customer(data[1])
+            if data[0] == '7':
+                res = get_data_for_print_report()
+            if data[0] == '8':
+                print(data[1])
+                self.request.sendall(str(data).encode())
+                break;
+            if isinstance(res, list):
+                self.request.sendall(str(res).encode())
+            else:
+                self.request.sendall(res.encode())
+
+
+    # def update_customer_age():
+    #     pass
+    #
+    #
+    # def update_customer_address():
+    #     pass
+    #
+    #
+    # def update_customer_phone():
+    #     pass
+    #
+    #
+
+
+def find_customer(cust_name):
+    if cust_name in PYTHON_DB:
+        return PYTHON_DB[cust_name]
+    else:
+        return 'Customer not found...'
+
+
+def add_customer(cust_data):
+    if cust_data[0] in PYTHON_DB:
+        return 'Customer already exists...'
+    else:
+        PYTHON_DB[cust_data[0]] = cust_data[1]
+    return cust_data
+
+
+def delete_customer(cust_name):
+    if cust_name in PYTHON_DB:
+        del PYTHON_DB[cust_name]
+    else:
+        return 'Customer does not exist...'
+    return [cust_name]
+
+
+def get_data_for_print_report():
+    sorted_data = []
+    for customer in sorted(PYTHON_DB.keys()):
+        record = [customer]
+        record.extend(PYTHON_DB[customer])
+        sorted_data.append(record)
+    return sorted_data
+
+
+# def check_age():
+#     pass
+#
+#
+# def check_address():
+#     pass
+#
+#
+# def check_phone():
+#     pass
+
+
 def is_valid_record(split_record):
     if split_record[0].strip().isnumeric():
         return False
     return True
-    # if len(split_record) == 2:
-    #     check_age()
-    # elif len(split_record) == 3:
-    #     check_age()
-    #     check_address()
-    # elif len(split_record) == 4:
-    #     check_age()
-    #     check_address()
-    #     check_phone()
 
 
 def add_record_to_db(split_record):
@@ -39,83 +120,14 @@ def load_data_in_memory():
 
 def create_server():
     HOST, PORT = "localhost", 9999
-
     # Create the server, binding to localhost on port 9999
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
+        print('Server Started...')
         server.serve_forever()
-
-
-# def check_age():
-#     pass
-#
-#
-# def check_address():
-#     pass
-#
-#
-# def check_phone():
-#     pass
-def find_customer():
-    pass
-
-
-def add_customer():
-    pass
-
-
-def delete_customer():
-    pass
-
-
-def update_customer_age():
-    pass
-
-
-def update_customer_address():
-    pass
-
-
-def update_customer_phone():
-    pass
-
-
-def print_report():
-    sorted_data = []
-    for customer in sorted(PYTHON_DB.keys()):
-        record = [customer]
-        record.extend(PYTHON_DB[customer])
-        sorted_data.append(record)
-
-    dash = '-' * 90
-    print(dash)
-    print('{:<25s}{:<10s}{:<40s}{:<12s}'.format("Name", "Age", "Street Address", "Phone #"))
-    print(dash)
-    for i in range(len(sorted_data)):
-        print('{:<25s}{:<10s}{:<40s}{:<12s}'.format(sorted_data[i][0], sorted_data[i][1], sorted_data[i][2],
-                                                    sorted_data[i][3]))
 
 
 if __name__ == "__main__":
     load_data_in_memory()
-    print_report()
-    # create_server()
-
-
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+    create_server()
